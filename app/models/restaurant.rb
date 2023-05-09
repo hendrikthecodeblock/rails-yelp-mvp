d93d2fde49e4116973e85f6fcf4ab448793f39a8
Hendrik Oosthuizen <hendrik@thecodeblock.net>
2023-05-09 12:14:24 +0200

    add rating to reviews table

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-  # has_many :reviews, dependant: :destroy
+  has_many :reviews, dependent: :destroy
   validates :name, :address, :category, presence: true
   validates :category, exclusion: {in: %w(neptunian), message: "%{value} is not allowed."}
 end
